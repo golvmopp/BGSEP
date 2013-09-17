efe766b683bb2f04a98f37b81c00e07699c6be2b
Patrik <dollarmannen@gmail.com>
2013-09-17 10:54:39 +0200

    Added Gyro and Joystick classes

---
+++ VirtualGamepad/src/bgsep/model/Gyro.java
@@ -0,0 +1,77 @@
+package bgsep.model;
+
+import java.util.Observable;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+public class Gyro extends Observable implements SensorEventListener{
+
+	private SensorManager 	sensorManager;
+	private Sensor 			sensorAccelerometer;
+	private float			indication;
+	private boolean			enabled;
+	
+	/**
+	 * Initializes the gyro. Enabled by default.
+	 * @param manager An initialized SensorManager.
+	 */
+	public Gyro(SensorManager manager)
+	{
+		sensorManager 		= manager;
+		sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		registerListener();
+		indication 		= 0;
+	}
+	
+	public void registerListener()
+	{
+		sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	public void unregisterListener()
+	{
+		sensorManager.unregisterListener(this);
+	}
+	
+	public float getIndication()
+	{
+		return indication;
+	}
+	
+	public float getMaximumRange()
+	{
+		return sensorAccelerometer.getMaximumRange();
+	}
+
+	public boolean getEnabled()
+	{
+		return enabled;
+	}
+	
+	public void setEnabled(boolean enabled)
+	{
+		this.enabled = enabled;
+	}
+	
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(enabled)
+			if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+			{
+				indication = event.values[1];
+				setChanged();
+				notifyObservers();
+			}
+	}
+	
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		
+	}
+
+}
---
+++ VirtualGamepad/src/bgsep/model/Joystick.java
@@ -0,0 +1,106 @@
+package bgsep.model;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+
+
+/**
+ * Handles joystick movements.
+ * @author patrik
+ *
+ */
+public class Joystick {
+
+	private ImageView boundary;
+	private ImageView stick;
+	private float prevPosX;
+	private float prevPosY;
+	private float startPointX;
+	private float startPointY;
+	private boolean enabled;
+	
+	/**
+	 * Takes a positioned boundary ImageView and stick ImageView. Enabled by default.
+	 * @param boundary an ImageView
+	 * @param stick an ImageView
+	 */
+	public Joystick(ImageView boundary, ImageView stick)
+	{
+		prevPosX		= 0;
+		prevPosY 		= 0;
+		startPointX 	= stick.getX();
+		startPointY 	= stick.getY();
+		boundary.setOnTouchListener(new JoystickTouchEvent());
+		
+		this.boundary 	= boundary;
+		this.stick 		= stick;
+		
+		enabled = true;
+	}
+	
+	private class JoystickTouchEvent implements OnTouchListener
+	{
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			
+			if(enabled)
+			{
+				switch(event.getAction())
+				{
+				case MotionEvent.ACTION_MOVE:
+				
+					int maxTop, maxRight, maxBottom, maxLeft;
+					maxTop = 0;
+					maxRight = boundary.getWidth();
+					maxLeft = 0;
+					maxBottom = boundary.getHeight();
+								
+				
+					if((event.getX() < maxLeft || event.getX() > maxRight) && 
+							(event.getY() > maxTop && event.getY() < maxBottom))
+						moveJoystick(prevPosX, event.getY());
+					else if((event.getY() < maxTop || event.getY() > maxBottom) &&
+							(event.getX() > maxLeft && event.getX() < maxRight))
+						moveJoystick(event.getX(), prevPosY);
+					else if(!(event.getX() < maxLeft || event.getX() > maxRight || 
+								event.getY() < maxTop || event.getY() > maxBottom))
+						moveJoystick(event.getX(), event.getY());
+				
+					break;
+				
+				case MotionEvent.ACTION_UP:
+					stick.setX(startPointX);
+					stick.setY(startPointY);
+				}
+				return true;
+			}			
+			return false;
+		}
+		
+		private void moveJoystick(float XPos, float YPos)
+		{
+			stick.setX(boundary.getLeft() + XPos - stick.getWidth()/2);
+			stick.setY(boundary.getTop() + YPos - stick.getHeight()/2);
+		
+			//Update previous position
+			prevPosX = XPos;
+			prevPosY = YPos;
+		}
+		
+	}
+	
+	public boolean getEnabled()
+	{
+		return enabled;
+	}
+	
+	public void setEnabled(boolean enabled)
+	{
+		this.enabled = enabled;
+	}
+	
+}
