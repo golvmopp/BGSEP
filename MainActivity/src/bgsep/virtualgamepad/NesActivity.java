168c02cea5e1c9538055c2d8e54a4e9019d6bcfe
Victor Olausson <vobc@live.com>
2013-09-19 12:02:12 +0200

    kontroller med alla knappar p fel plats

---
+++ MainActivity/src/bgsep/virtualgamepad/NesActivity.java
@@ -0,0 +1,15 @@
+package bgsep.virtualgamepad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nes);
+	}
+	
+	
+}
