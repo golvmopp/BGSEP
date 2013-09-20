cf07d7bb0aef715a9cb40b0bdefd2178c811c3ca
Isak Eriksson <isak.eriksson@mail.com>
2013-09-20 14:42:13 +0200

    fixing layout

--- VirtualGamepad/src/bgsep/virtualgamepad/NesActivity.java
+++ VirtualGamepad/src/bgsep/virtualgamepad/NesActivity.java
@@ -1,6 +1,7 @@
 package bgsep.virtualgamepad;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class NesActivity extends Activity {
@@ -9,6 +10,7 @@ public class NesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nes);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 	}
 	
 	
