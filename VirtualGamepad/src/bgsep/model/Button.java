ececa308d54b1e339dabe52176b3a9fa60e4e2ad
Patrik <dollarmannen@gmail.com>
2013-09-25 18:31:16 +0200

    Updated the Button class to support future communication with computers. Also added easier changing of the image for when it is pressed or not pressed

--- VirtualGamepad/src/bgsep/model/Button.java
+++ VirtualGamepad/src/bgsep/model/Button.java
@@ -1,9 +1,10 @@
 package bgsep.model;
 
+import java.nio.ByteBuffer;
 import java.util.Observable;
 import java.util.Observer;
 
-import android.util.Log;
+import android.graphics.drawable.Drawable;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -11,22 +12,40 @@ import android.widget.ImageView;
 
 
 public class Button extends Observable {
-	
-	/* **** Button Identifiers **** */
-	public static final int BUTTON_0 = 0;
-	public static final int BUTTON_1 = 1;
-	public static final int BUTTON_A = 2;
-	public static final int BUTTON_B = 3;
-	/* **************************** */
 
-	private int buttonIdentifier;
+	private int buttonKeyCode;
+	private ImageView buttonView;
 	private boolean isPressed;
+	private int pressedDrawableID, unPressedDrawableID;
 	
-	public Button(ImageView button, int identifier, Observer obs) {
-		buttonIdentifier = identifier;
+	
+	public Button(ImageView button, int unPressedDrawableID, int pressedDrawableID,
+			int keyCode, Observer o) {
+		this(button, unPressedDrawableID, pressedDrawableID);
+		buttonKeyCode = keyCode;
+		addObserver(o);
+	}
+	
+	public Button(ImageView button, int unPressedDrawableID, int pressedDrawableID) {
+		buttonKeyCode = 0;
 		button.setOnTouchListener(new ButtonTouchEvent());
-		addObserver(obs);
+		this.pressedDrawableID = pressedDrawableID;
+		this.unPressedDrawableID = unPressedDrawableID;
 		isPressed = false;
+		buttonView = button;
+	}
+	
+	public void setButtonKeyCode(int keyCode) {
+		buttonKeyCode = keyCode;
+	}
+	
+	
+	public ImageView getButtonView() {
+		return buttonView;
+	}
+
+	public boolean isPressed() {
+		return isPressed;
 	}
 	
 	private class ButtonTouchEvent implements OnTouchListener {
@@ -37,28 +56,41 @@ public class Button extends Observable {
 			switch(event.getAction()) {
 			case MotionEvent.ACTION_DOWN:
 				isPressed = true;
-				
-				//Notify observers
 				setChanged();
-				notifyObservers(buttonIdentifier);
+				notifyObservers(convertToByteBuffer(buttonKeyCode, isPressed));
 				break;
 			case MotionEvent.ACTION_UP:
 				isPressed = false;
-				
-				//Notify observers
 				setChanged();
-				notifyObservers(buttonIdentifier);
+				notifyObservers(convertToByteBuffer(buttonKeyCode, isPressed));
 				break;
 			default:
-				break;
+				break;	
 			}
+			
 			return true;
 		}
 		
 	}
 	
-	public boolean isPressed() {
-		return isPressed;
+	private ByteBuffer convertToByteBuffer(int value, boolean press) {
+		ByteBuffer byteBuffer = ByteBuffer.allocate(32);
+		byteBuffer.putInt(value);
+		
+		// Set first bit to 1 to indicate a keyPress, else its 0 by default
+		// and indicate a keyRelease
+		if(press)
+			byteBuffer.array()[0] = 1;
+		
+		return byteBuffer;
+	}
+	
+	public int getPressedDrawableID() {
+		return pressedDrawableID;
+	}
+	
+	public int getUnPressedDrawableID() {
+		return unPressedDrawableID;
 	}
 
 }
--- VirtualGamepad/src/bgsep/virtualgamepad/NesActivity.java
+++ VirtualGamepad/src/bgsep/virtualgamepad/NesActivity.java
@@ -4,6 +4,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 import bgsep.model.Button;
+import bgsep.virtualgamepad.R.drawable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,36 +21,23 @@ public class NesActivity extends Activity implements Observer {
 		
 		imageAbutton = (ImageView)findViewById(R.id.imageAbutton);
 		imageBbutton = (ImageView)findViewById(R.id.imageBbutton);
-		
-		new Button(imageAbutton, Button.BUTTON_A, this);
-		new Button(imageBbutton, Button.BUTTON_B, this);
+		new Button(imageAbutton, R.drawable.a_button, R.drawable.a_button_pressed,
+				45, this);
+		new Button(imageBbutton, R.drawable.b_button, R.drawable.b_button_pressed,
+				46, this);
 		
 	}
 
 	@Override
 	public void update(Observable o, Object obj) {
-		if(o instanceof Button && obj instanceof Integer) {
+		if(o instanceof Button) {
 			Button button = (Button)o;
-			int buttonIdentifier = (Integer)obj;
-
-			switch(buttonIdentifier) {
-			case Button.BUTTON_A:
-				if(button.isPressed())
-					imageAbutton.setImageResource(R.drawable.a_button_pressed);
-				else
-					imageAbutton.setImageResource(R.drawable.a_button);
-				break;
+			
+			if(button.isPressed())
+				button.getButtonView().setImageResource(button.getPressedDrawableID());
+			else
+				button.getButtonView().setImageResource(button.getUnPressedDrawableID());
 				
-			case Button.BUTTON_B:
-				if(button.isPressed()) 
-					imageBbutton.setImageResource(R.drawable.b_button_pressed);
-				else
-					imageBbutton.setImageResource(R.drawable.b_button);
-				break;
-				
-			default:
-				break;
-			}
 		}
 	}
 	
