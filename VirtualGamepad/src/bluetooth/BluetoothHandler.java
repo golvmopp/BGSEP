3ff0cfff079179845e47572d944202d8a3278b50
linlind <linuslindgren@live.com>
2013-09-25 20:28:28 +0200

    BluetoothHandler now sets up a GATTServer and connects to paired device

--- VirtualGamepad/src/bluetooth/BluetoothHandler.java
+++ VirtualGamepad/src/bluetooth/BluetoothHandler.java
@@ -1,72 +1,158 @@
 package bluetooth;
 
-import java.util.HashMap;
+import java.util.UUID;
 
-import bgsep.virtualgamepad.MainActivity;
-import bgsep.virtualgamepad.R;
-import android.R.layout;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.nsd.NsdManager.RegistrationListener;
+import android.content.pm.PackageManager;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
 
+
+@TargetApi(18)
 public class BluetoothHandler {
+	byte[] gamePadDescriptor = {
+			
+			0x05, 0x01, //; USAGE_PAGE (Generic Desktop)
+			 0x09, 0x05, //; USAGE (Gamepad)
+			 (byte) 0xa1, 0x01, //; COLLECTION (Application)
+			 0x05, 0x09,// ; USAGE_PAGE (Button)
+			 0x19, 0x01, //; USAGE_MINIMUM (Button 1)
+			 0x29, 0x09, //; USAGE_MAXIMUM (Button 9)
+			 0x15, 0x00, //; LOGICAL_MINIMUM (0)
+			 0x25, 0x01, //; LOGICAL_MAXIMUM (1)
+			 0x75, 0x01, //; REPORT_SIZE (1)
+			 (byte) 0x95, 0x09, //; REPORT_COUNT (9)
+			 (byte) 0x81, 0x02, //; INPUT (Data,Var,Abs)
+			 (byte) 0x95, 0x07, // REPORT_COUNT (7)
+			 (byte) 0x81, 0x03, // INPUT (Cnst,Var,Abs)
+			 (byte) 0xC0// ; END_COLLECTION
+			
+	};
 	
-	private HashMap<String, String> deviceMap;
 	private Activity activity;
 	private static final String TAG = "Gamepad";
-	private TextView text;
+	private BluetoothManager manager;
+	private BluetoothAdapter adapter;
+
+	
+	public static String HID_UUID = "00001124-0000-1000-8000-00805f9b34fb";
 	
 	public BluetoothHandler(Activity activity) {
-		text = (TextView)activity.findViewById(R.id.log);
-		deviceMap = new HashMap<String, String>();
+		
 		this.activity = activity;
-		BluetoothAdapter ba = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
-		if (ba == null) {
-			log("No bluetooth adapter detected!");
+
+		manager = (BluetoothManager)activity.getSystemService(Context.BLUETOOTH_SERVICE);
+
+		adapter = manager.getAdapter();
+
+		if (adapter == null) {
+			Log.d(TAG,"No bluetooth adapter detected!");
 			return;
 		} else {
-			log("Bluetooth adapter \"" + ba.getName() + "\" detected");
+			Log.d(TAG,"Bluetooth adapter \"" + adapter.getName() + "\" detected");
 		}
-		if (ba.isEnabled()) {
-			log("Bluetooth device is enabled");
+		if (adapter.isEnabled()) {
+			Log.d(TAG,"Bluetooth device is enabled");
 		} else {
-			log("Bluetooth device is disabled");
-			log("Enabling bluetooth device..");
-			log(ba.enable() ? "Success" : "Failed");
+			Log.d(TAG,"Bluetooth device is disabled");
+			Log.d(TAG,"Enabling bluetooth device..");
+			Log.d(TAG,adapter.enable() ? "Success" : "Failed");
 		}
-		log("Starting discovery...");
-		if (ba.startDiscovery()) {
-			log("Discovering...");
-			IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-			activity.registerReceiver(mReciever, filter);
-		} else {
-			log("Discovering did not start?");
+		
+
+		
+		BluetoothGattServerCallback bgsc = new BluetoothGattServerCallback() {
+		};
+		
+		BluetoothGattServer gattServer = manager.openGattServer(activity, bgsc);
+		UUID hid = UUID.fromString(HID_UUID);
+
+		BluetoothGattService hidService = new BluetoothGattService(hid, BluetoothGattService.SERVICE_TYPE_PRIMARY);
+		
+		BluetoothGattCharacteristic charact = new BluetoothGattCharacteristic(UUID.randomUUID(), BluetoothGattCharacteristic.FORMAT_UINT16, BluetoothGattCharacteristic.PERMISSION_READ);
+		
+		BluetoothGattDescriptor desc = new BluetoothGattDescriptor(UUID.randomUUID(), BluetoothGattDescriptor.PERMISSION_READ);
+		desc.setValue(gamePadDescriptor);
+		
+		charact.addDescriptor(desc);
+		
+		hidService.addCharacteristic(charact);
+		
+		if(gattServer.addService(hidService)){
+			Log.d(TAG, "SUCCEEDED ADDING HID SERVICE");
+		}else{
+			Log.d(TAG, "FAILED ADDING HID SERVICE");
 		}
-		log("Discovery done");
+		
+		if(!activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+			Log.d(TAG, "FAIL");
+		}else{
+			Log.d(TAG, "WIN");
+		}
+
+		for(BluetoothGattService s : gattServer.getServices()){
+			Log.d(TAG, "SERVICE: " + s.getUuid());
+			Log.d(TAG, "HAS INCLUDED SERVICES: " + s.getIncludedServices().size());
+
+			for(BluetoothGattCharacteristic c : s.getCharacteristics()){
+				Log.d(TAG, "CHARACTERISTIC " + c.getInstanceId() + "\n\tWITH PERMISION " + c.getPermissions() + "\n\tWITH PROPERTIES " + c.getProperties());
+				
+				for(BluetoothGattDescriptor d : c.getDescriptors()){
+					Log.d(TAG, "DECRIPTOR " + d.getUuid() + " WITH VALUES " + d.getValue().toString());
+				}
+			}
+			
+		}
+		BluetoothDevice device = null;
+		
+		BluetoothGattCallback callback = new BluetoothGattCallback() {
+			@Override
+			public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState){
+				Log.d(TAG, "status: " + status + ", newState: " + newState);
+			}
+		};
+		
+		if(adapter.getBondedDevices() != null && adapter.getBondedDevices().size() != 0){
+			Log.d(TAG, adapter.getBondedDevices().size() + " bounded devices");
+			for(BluetoothDevice d : adapter.getBondedDevices()){
+				Log.d(TAG, "\t" + d.getName());
+				BluetoothGatt bg = d.connectGatt(activity, true, callback);
+				Log.d(TAG, "CONNECT: ");
+				Log.d(TAG, "CONNECTION STATE FOR DEVICE " + manager.getConnectionState(d, BluetoothProfile.GATT));
+
+				device = d;
+			}
+		}
+
+
+		while(true){
+			for(BluetoothDevice bd : manager.getConnectedDevices(BluetoothProfile.GATT)){
+				Log.d(TAG,"SENDING RESPONSE");
+				gattServer.sendResponse(bd, 0, 0, 0, this.gamePadDescriptor);
+			}
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+       // LeDeviceScanActivity scanActivity = new LeDeviceScanActivity();
+		//Log.d(TAG, "STARTING LE SCAN: " + scanActivity.scan(adapter));
+        
 		
 	}
 	
-	private void log(String text) {
-		this.text.setText(this.text.getText() + "\n" + text);
-	}
-	
-	private final BroadcastReceiver mReciever = new BroadcastReceiver() {
-		
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-				deviceMap.put(device.getName(), device.getAddress());
-			}	
-		}
-	};
 }
