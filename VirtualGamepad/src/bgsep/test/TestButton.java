467b57e4bd9dc74882c53b64c53d23711aa469fe
William Dahlberg <github@golvmopp.it>
2013-10-01 11:39:28 +0200

    Created test package

---
+++ VirtualGamepad/src/bgsep/test/TestButton.java
@@ -0,0 +1,37 @@
+package bgsep.test;
+
+import junit.framework.TestCase;
+
+public class TestButton extends TestCase {
+
+	public final void testButtonImageViewIntIntIntObserver() {
+		assertEquals(expected, actual);
+		asser
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testButtonImageViewIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetButtonKeyCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetButtonView() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsPressed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetPressedDrawableID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetUnPressedDrawableID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
