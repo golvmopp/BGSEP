ff9a4f57a4a4d11c51d76949ab678864838ad106
Martin <marljung@student.chalmers.se>
2013-10-02 22:07:04 +0200

    Complete GC added, works in simulator and on S Galaxy S2 + minor buggfix in NES

--- VirtualGamepad/src/bgsep/virtualgamepad/MainActivity.java
+++ VirtualGamepad/src/bgsep/virtualgamepad/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_nes);
+		setContentView(R.layout.activity_gc);
 		BluetoothHandler bh = null;
 		if(android.os.Build.VERSION.SDK_INT == android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
 			bh = new BluetoothHandler(this);
