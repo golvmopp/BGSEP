2645a2185d35370fad467be214a0500ca7be7c05
Isak Eriksson <isak.eriksson@mail.com>
2013-10-04 14:15:58 +0200

    made KeyMap class final and it's method static

--- VirtualGamepadHost/src/host/KeyMap.java
+++ VirtualGamepadHost/src/host/KeyMap.java
@@ -3,8 +3,8 @@ package host;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class KeyMap {
-	private ArrayList<Integer> keyCodes;
+public final class KeyMap {
+	private static ArrayList<Integer> keyCodes;
 	
 	private static final int KEYS_PER_CLIENT = 20;
 	
@@ -12,10 +12,9 @@ public class KeyMap {
 	private static final int MY_CALCULATOR = 183;
 	private static final int NUM_LOCK = 144;
 	private static final int SCROLL_LOCK = 145;
-	
-	public KeyMap() {
+
+	static {
 		keyCodes = new ArrayList<Integer>();
-		
 		for (int i = 124; i <= 149; i++) {
 			if (i != MY_COMPUTER && i != MY_CALCULATOR && i != NUM_LOCK && i != SCROLL_LOCK) { 
 				keyCodes.add(i);
