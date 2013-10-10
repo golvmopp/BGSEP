306edf0ee9494dd5643c2dc9b096723e78412072
linlind <linuslindgren@live.com>
2013-10-10 16:10:49 +0200

    Added user input for number of clients and number of buttons at the server start. Added implementation for "list and " "reloadConfiguration" in the server terminal.
    The server now handles different config files with different setups (number of clients and number of buttons)

--- VirtualGamepadHost/src/host/Configuration.java
+++ VirtualGamepadHost/src/host/Configuration.java
@@ -1,22 +1,24 @@
 package host;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import bluetooth.BluetoothServer;
+
 public class Configuration {
-	private static final int DEFAULT_NUMBER_OF_CLIENTS = 2;
 	private int numberOfClients;
-	private static final int DEFAULT_NUMBER_OF_BUTTONS = 15;
 	private int numberOfButtons;
-	private static final int MY_COMPUTER = 182;
-	private static final int MY_CALCULATOR = 183;
-	private static final int NUM_LOCK = 144;
-	private static final int SCROLL_LOCK = 145;
+
 	private File configFile;
 	private String configuration;
 	private static ArrayList<Integer> keyCodes;
 	private static Configuration instance = null;
-	
+
 	public synchronized static Configuration getInstance() {
 		if (instance == null) {
 			instance = new Configuration();
@@ -24,61 +26,102 @@ public class Configuration {
 		return instance;
 	}
 	
-	private Configuration() {
+	/**
+	 * Gets the configured key code for a specific client and a button.
+	 * 
+	 * @param clientID
+	 *            the client on which a button was pressed/released
+	 * @param buttonID
+	 *            the button which was pressed/released
+	 * @return the configured key code
+	 */
+	public synchronized int getKeyCode(int clientID, int buttonID) {
+		int index = clientID * numberOfClients + buttonID;
+		if (index < keyCodes.size()) {
+			return keyCodes.get(clientID * numberOfButtons + buttonID);
+		} else {
+			throw new IllegalArgumentException("clientID " + clientID + " is too big!");
+		}
+	}
+	/**
+	 * 
+	 * Takes input from the user about number of clients and number of buttons. Then parses the matching
+	 * configuration file (Creates a new one if needed).
+	 * The configuration files is located at '~/.config/VirtualGamepad'.
+	 * 
+	 */
+	
+	public void loadConfig(){
+
+		numberOfClients = BluetoothServer.getNumberOfClients();
+		numberOfButtons = BluetoothServer.getNumberOfButtons();
 		keyCodes = new ArrayList<Integer>();
-		determineConfigFileLocation(); //different path on different operating systems
+		determineConfigFileLocation(); // different path on different operating
+										// systems
 		if (!configFile.exists()) {
 			generateConfiguration();
 			writeConfigFile();
 		}
 		parseConfigFile();
+	
+	}
+
+
+	private Configuration() {
+		loadConfig();
 	}
 	
 	private void addDefaultKeyCodes() {
 		/*
-		for (int i = 1; i <= 7; i++) {
+		 * for (int i = 1; i <= 7; i++) { keyCodes.add(i); } for (int i = 21; i
+		 * <= 26; i++) { keyCodes.add(i); } for (int i = 124; i <= 249; i++) {
+		 * if (i != MY_COMPUTER && i != MY_CALCULATOR && i != NUM_LOCK && i !=
+		 * SCROLL_LOCK) { keyCodes.add(i); } }
+		 */
+		for (int i = 65; i <= 249; i++) {
 			keyCodes.add(i);
 		}
-		for (int i = 21; i <= 26; i++) {
-			keyCodes.add(i);
+
+		while (keyCodes.size() < 240) {
+			keyCodes.add(0);
 		}
-		for (int i = 124; i <= 249; i++) {
-			if (i != MY_COMPUTER && i != MY_CALCULATOR && i != NUM_LOCK && i != SCROLL_LOCK) { 
-				keyCodes.add(i);
-			}
-		}*/
-		for (int i = 65; i <= 111; i++) {
-			keyCodes.add(i);
-		}
-		
+
 	}
-	
+
 	private void determineConfigFileLocation() {
 		String os = System.getProperty("os.name");
 		if (os.startsWith("Linux")) {
-			System.out.println("you are running on a Linux machine, setting config location to ~/.config/");
-			configFile = new File(System.getProperty("user.home") + File.separatorChar + ".config" + File.separatorChar + "virtual-gamepad.conf");
-			
+			System.out.println("you are running on a Linux machine, setting config location to ~/.config/VirtualGamepad");
+			configFile = new File(System.getProperty("user.home") + File.separatorChar + ".config/VirtualGamepad" + File.separatorChar + "virtual-gamepad-"+ numberOfClients +"c" + numberOfButtons +"b.conf");
+			System.out.println("config file path \"" + configFile.getAbsolutePath()+ "\"");
 		} else if (os.startsWith("Windows")) {
 			System.out.println("Windoze is not yet supported. Get a Linux based OS now!");
 			System.exit(1);
 		}
 	}
-	
+
 	private void generateConfiguration() {
 		addDefaultKeyCodes();
 		System.out.println("generating config file...");
-		String general = "NumberOfClients=" + DEFAULT_NUMBER_OF_CLIENTS;
 		StringBuilder buttonKeyCodes = new StringBuilder();
-		for (int client = 0; client < DEFAULT_NUMBER_OF_CLIENTS; client++) {
-			for (int button = 0; button < DEFAULT_NUMBER_OF_BUTTONS; button++) {
-				buttonKeyCodes.append("client" + client + ":button" + button + "=" + keyCodes.get(client * DEFAULT_NUMBER_OF_BUTTONS + button) + "\n");
+		for (int client = 0; client < numberOfClients; client++) {
+			for (int button = 0; button < numberOfButtons; button++) {
+				int code = client * numberOfButtons + button;
+				buttonKeyCodes.append("client" + client + ":button" + button + "=" + keyCodes.get(code) + "\n");
+				if (keyCodes.get(code) == 249) {
+					System.out.println();
+					System.out.println("---------------------------------------------------------");
+					System.out.println("NOTIFICATION: THE NUMBER OF DEFAULT KEYCODES HAS EXCEEDED!" + "\nIn \"" + configFile.getAbsolutePath() + "\"\nat [client" + client + ", button" + button + "]"
+							+ "\nPLEASE MANUALLY REPLACE ALL ZEROES IN THE CONFIG FILE");
+					System.out.println("---------------------------------------------------------");
+
+				}
 			}
 		}
-		this.configuration = "[General]\n" + general + "\n\n[KeyCodes]\n" + buttonKeyCodes.toString();
+		this.configuration = "[KeyCodes]\n" + buttonKeyCodes.toString();
 		System.out.println("done");
 	}
-	
+
 	private void writeConfigFile() {
 		System.out.println("writing new configuration...");
 		try {
@@ -94,13 +137,13 @@ public class Configuration {
 		}
 		System.out.println("done");
 	}
-	
+
 	private void parseNumberOfClients(String line) {
 		int number = Integer.parseInt(line.split("=")[1]);
 		System.out.println("setting number of clients to " + number);
 		numberOfClients = number;
 	}
-	
+
 	private void parseLine(String line) {
 		if (line.contains("NumberOfClients")) {
 			parseNumberOfClients(line);
@@ -110,12 +153,13 @@ public class Configuration {
 				client = getClient(line);
 				button = getButton(line);
 				code = getCode(line);
-				System.out.println("client " + client + " : button " + button + " : code " + code);
+				// System.out.println("client " + client + " : button " + button
+				// + " : code " + code);
 				keyCodes.add(client * numberOfClients + button, code);
 			}
 		}
 	}
-	
+
 	private void parseConfigFile() {
 		System.out.println("parsing config file...");
 		String line = "";
@@ -143,14 +187,14 @@ public class Configuration {
 		}
 		System.out.println("done");
 	}
-	
+
 	private int getClient(String line) {
 		line = line.split(":")[0];
 		line = line.split("client")[1];
 		int number = Integer.parseInt(line);
 		return number;
 	}
-	
+
 	private int getButton(String line) {
 		line = line.split(":")[1];
 		line = line.split("button")[1];
@@ -158,25 +202,12 @@ public class Configuration {
 		int number = Integer.parseInt(line);
 		return number;
 	}
-	
+
 	private int getCode(String line) {
 		line = line.split("=")[1];
 		int number = Integer.parseInt(line);
 		return number;
 	}
-	
-	/**
-	 * Gets the configured key code for a specific client and a button. 
-	 * @param clientID the client on which a button was pressed/released
-	 * @param buttonID the button which was pressed/released
-	 * @return the configured key code
-	 */
-	public synchronized int getKeyCode(int clientID, int buttonID) {
-		int index = clientID * DEFAULT_NUMBER_OF_CLIENTS + buttonID;
-		if (index < keyCodes.size()) {
-			return keyCodes.get(clientID * DEFAULT_NUMBER_OF_BUTTONS + buttonID);
-		} else {
-			throw new IllegalArgumentException("clientID " + clientID + " is too big!");
-		}
-	}
+
+
 }
--- VirtualGamepadHost/src/host/Main.java
+++ VirtualGamepadHost/src/host/Main.java
@@ -6,8 +6,8 @@ import bluetooth.BluetoothServer;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("VIRTUAL GAMEPAD HOST");
-		Configuration configuration = Configuration.getInstance();
-		BluetoothServer bs = new BluetoothServer();
+		Configuration.getInstance();
+		new BluetoothServer();
 		Terminal terminal = new Terminal();
 		terminal.start();
 	} 
